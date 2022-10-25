9f6ebad64eede20360d06fc505e4e9dbd906e1ee
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-25 13:12:23 +0300

    completed task

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,9 @@
-require 'pry'
 def oxford_comma(array)
     if array.length ==1
-        array.pop
+        array.sample
     elsif array.length == 2
         array.join(" and ")
     else
         array[0..-2].join(", ") + ", and " + array[-1]
     end
 end
-binding.pry
